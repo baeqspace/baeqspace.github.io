8db2e73682285b3ce62a6382aeae933533624280
baeqspace <sashmus.04@gmail.com>
2023-01-07 09:50:03 +0300

    my works

---
+++ Appello/script.js
@@ -0,0 +1,29 @@
+let scroller = document.querySelector('.kick-3')
+let left = document.querySelector('.button-left')
+let right = document.querySelector('.button-right')
+let maxScroll = scroller.scrollWidth - scroller.clientWidth
+
+const move = (isRight) => {
+    scroller.scrollBy(isRight ? 300 : -300,0)
+}
+
+let interval;
+
+const setInterv = () => {
+    interval = setInterval(()=> {
+        scroller.scrollLeft >= maxScroll ? scroller.scroll(0,0) : move(true)
+    },1000)
+}
+
+let timer;
+
+const setTimer = () => {
+    clearInterval(interval)
+    clearTimeout(timer)
+    timer = setTimeout(setInterv, 5000)
+}
+
+left.onclick = () => {setTimer();move(false)}
+right.onclick = () => {setTimer();move(true)}
+
+setInterv()
---
+++ Evarugs/script.js
@@ -0,0 +1,41 @@
+let slider = document.querySelector('.reviews-container')
+let maxScroll = slider.scrollWidth - slider.clientWidth
+
+const move = (isRight) => {
+    slider.scrollBy(isRight ? 300 : -300,0)
+}
+
+let interval;
+
+const setInterv = () => {
+    interval = setInterval(()=> {
+        slider.scrollLeft >= maxScroll ? slider.scroll(0,0) : move(true)
+    },2000)
+}
+
+let timer;
+
+const setTimer = () => {
+    clearInterval(interval)
+    clearTimeout(timer)
+    timer = setTimeout(setInterv, 5000)
+}
+
+setInterv()
+
+let sliderElems = slider.querySelectorAll('.review')
+let dotsCont = document.querySelector('.dots')
+let k = 1
+for (let i of sliderElems) {
+    let dot = document.createElement('div')
+    dot.classList.add('dot')
+    dot.dataset.count = k
+    dotsCont.append(dot)
+    dot.onclick = () => {
+        setTimer()
+        let pos = Math.round(maxScroll / sliderElems.length * dot.dataset.count) - 200
+        slider.scroll(pos, 0)
+        console.log(pos)
+    }
+    k++
+}
