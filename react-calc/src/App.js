44152c3032851905d54cbdf370bd171f544cef9e
baeqspace <sashmus.04@gmail.com>
2023-02-24 14:27:46 +0300

    new project in work

---
+++ react-calc/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Calc'>
+        <div className='result'></div>
+        <div className='button'>C</div>
+        <div className='button'>c</div>
+        <div className='button'>%</div>
+        <div className='button'>/</div>
+        <div className='button'>7</div>
+        <div className='button'>8</div>
+        <div className='button'>9</div>
+        <div className='button'>*</div>
+        <div className='button'>4</div>
+        <div className='button'>5</div>
+        <div className='button'>6</div>
+        <div className='button'>-</div>
+        <div className='button'>1</div>
+        <div className='button'>2</div>
+        <div className='button'>3</div>
+        <div className='button'>+</div>
+        <div className='button'>000</div>
+        <div className='button'>0</div>
+        <div className='button'>.</div>
+        <div className='button'>=</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
